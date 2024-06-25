8a43cab1f17c5c4f2cc043b33a7482522690d2c5
harsha-krupashankar <75800001+harsha-krupashankar@users.noreply.github.com>
2024-06-25 10:07:30 +0530

    Initial commit

---
+++ dist/index.d.ts
@@ -0,0 +1,54 @@
+import z from "zod";
+export declare const signUpInput: z.ZodObject<{
+    email: z.ZodString;
+    password: z.ZodString;
+    name: z.ZodOptional<z.ZodString>;
+}, "strip", z.ZodTypeAny, {
+    email: string;
+    password: string;
+    name?: string | undefined;
+}, {
+    email: string;
+    password: string;
+    name?: string | undefined;
+}>;
+export declare const signInInput: z.ZodObject<{
+    email: z.ZodString;
+    password: z.ZodString;
+}, "strip", z.ZodTypeAny, {
+    email: string;
+    password: string;
+}, {
+    email: string;
+    password: string;
+}>;
+export declare const createBlogInput: z.ZodObject<{
+    title: z.ZodString;
+    content: z.ZodString;
+    published: z.ZodBoolean;
+}, "strip", z.ZodTypeAny, {
+    title: string;
+    content: string;
+    published: boolean;
+}, {
+    title: string;
+    content: string;
+    published: boolean;
+}>;
+export declare const updateBlogInput: z.ZodObject<{
+    title: z.ZodOptional<z.ZodString>;
+    content: z.ZodOptional<z.ZodString>;
+    published: z.ZodOptional<z.ZodBoolean>;
+}, "strip", z.ZodTypeAny, {
+    title?: string | undefined;
+    content?: string | undefined;
+    published?: boolean | undefined;
+}, {
+    title?: string | undefined;
+    content?: string | undefined;
+    published?: boolean | undefined;
+}>;
+export type SignUpInput = z.infer<typeof signUpInput>;
+export type SignInInput = z.infer<typeof signInInput>;
+export type CreateBlogInput = z.infer<typeof createBlogInput>;
+export type UpdateBlogInput = z.infer<typeof updateBlogInput>;
